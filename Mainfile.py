92ade390720bf6fc308c57fe1ecd7cc82cc171f2
BradenAkridge <165215649+BradenAkridge@users.noreply.github.com>
2024-03-27 17:28:50 -0400

    Create Mainfile.py
    The game file

---
+++ Mainfile.py
@@ -0,0 +1,7 @@
+print("your name is Jacob, and you just")
+print("woke up. you see a flashlight and")
+print("a door. whwt do you do?")
+choice1 = input()
+if choice1 == "door":
+  print("you go to the door, but darkness consumes you.)
+  print("hint: maybe get light")
